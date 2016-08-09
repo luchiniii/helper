88ab113a818d1f7533d023c9b9ac48b2668cc1c9
Luis Palomá <lpaloma@merqueo.com>
2016-08-09 16:49:39 -0500

    Finalización de la funcion

--- generador_cupones.php
+++ generador_cupones.php
@@ -1,4 +1,22 @@
 <?php
+function generatePIN($digits = 4){
+	$i = 0; //counter
+	$pin = ""; //our default pin is blank.
+	while($i < $digits){
+	//generate a random number between 0 and 9.
+		$pin .= mt_rand(0, 9);
+		$i++;
+	}
+	return $pin;
+}
 
+
+$string = 'philips';
+$num = 500;
+for ($i=0; $i < $num; $i++) { 
+	$digits = generatePIN(4);
+	$cupon = $string.$digits;
+	echo $cupon.'<br>';
+}
 
 ?>
